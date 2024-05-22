3eb454be00c38257ad00dd2505a56afa972e187a
MarioZ <mazc1995@gmail.com>
2024-05-21 20:43:44 -0500

    models relations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+item1 = Item.create(
+  name: 'Gafas de sol Carey',
+  item_type: 'Producto',
+  stock: 100,
+  price: 39.99,
+  thumbnail: 'url_a_la_imagen',
+  description: 'Gafas de sol con marco de carey.'
+)
+
+item2 = Item.create(
+  name: 'Red Hot Chili Peppers en Madrid',
+  item_type: 'Evento',
+  stock: 50,
+  price: 60.00,
+  thumbnail: 'url_a_la_imagen',
+  description: 'Concierto de Red Hot Chili Peppers en Madrid.'
+)
+
+cart = Cart.create(total: 0)
+
+CartItem.create(cart: cart, item: item1, quantity: 3, subtotal: item1.price * 3)
+CartItem.create(cart: cart, item: item2, quantity: 5, subtotal: item2.price * 5)
